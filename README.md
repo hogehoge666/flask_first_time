70a41243a0204ae06ddbc5ef4c20491d714da125
tnomura <hogehoge666@gmail.com>
2021-01-06 15:58:05 +0900

    implements on-memory ToDo app

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# 簡単なFlask Todoアプリ
+
+Flaskで作成する簡単なTodoアプリの写経です。
+
+## 1. 最初のコミット
+- flaskの最小アプリ作成
+- 初歩的なルーティング
+- Jinja2の基本的なTemplate機能
+- コンバーター
+- favicon.co表示
+
+## 2. ２番目のコミット
+- Todoアプリ（オンメモリ）作成
+- View関数から外部モジュール参照
+- form取得
+- Jinja2の分岐、繰り返し機能
+- CSS
+- PyCharm CEでFlaskのデバッグ構成
+- READMEの追加
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+from todo import ToDoList
 
 app = Flask(__name__)
 
+todolist = ToDoList()
 
 @app.route('/favicon.ico')
 def favicon():
@@ -9,21 +11,52 @@ def favicon():
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def show_todolist():
+    return render_template('showtodo.html', todolist=todolist.get_all())
 
 
-@app.route('/about')
-def about():
-    return '<h1>About:</h1>'
+@app.route('/additem', methods=['POST'])
+def add_item():
+    title = request.form['title']
+    if not title:
+        return redirect('/')
+    todolist.add(title)
+    return redirect('/')
 
 
-@app.route('/hello/<whom>')
-def hello(whom):
-    # return f'<h1>Hello {whom}</h1>'
-    return render_template('hello.html', whom=whom)
+@app.route('/deleteitem/<int:item_id>')
+def delete_todoitem(item_id):
+    todolist.delete(item_id)
+    return redirect('/')
 
 
-@app.route('/100_plus/<int:n>')
-def adder(n):
-    return f'100+{n}={100+n}'
+@app.route('/updatedone/<int:item_id>')
+def update_todoiitemdone(item_id):
+    todolist.update(item_id)
+    return redirect('/')
+
+
+@app.route('/deletealldoneitems')
+def delete_alldoneitems():
+    todolist.delete_doneitem()
+    return redirect('/')
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+#
+#
+# @app.route('/about')
+# def about():
+#     return '<h1>About:</h1>'
+#
+#
+# @app.route('/hello/<whom>')
+# def hello(whom):
+#     # return f'<h1>Hello {whom}</h1>'
+#     return render_template('hello.html', whom=whom)
+#
+#
+# @app.route('/100_plus/<int:n>')
+# def adder(n):
+#     return f'100+{n}={100+n}'
